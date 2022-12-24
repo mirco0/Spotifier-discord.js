1fc4bc413f309f9afd7517f051edd8bc8c7cbfb9
mirco0 <mircomicromac@gmail.com>
2022-12-24 12:33:27 +0100

    added spotify status reading

--- app.js
+++ app.js
@@ -1,45 +1,53 @@
 require('dotenv').config()
-const { Client, GatewayIntentBits, GatewayDispatchEvents, Events } = require('discord.js');
+const { Client, GatewayIntentBits, GatewayDispatchEvents, Events, Colors } = require('discord.js');
 
-const client = new Client({ intents: [GatewayIntentBits.GuildMessages,GatewayIntentBits.Guilds,GatewayIntentBits.GuildVoiceStates ] });
+const client = new Client({ intents: [GatewayIntentBits.GuildMessages,GatewayIntentBits.Guilds,GatewayIntentBits.GuildVoiceStates 
+    ,GatewayIntentBits.GuildPresences, GatewayIntentBits.MessageContent, GatewayIntentBits.GuildMembers]});
+
 const { generateDependencyReport, AudioPlayerStatus, createAudioPlayer, createAudioResource, getVoiceConnection, joinVoiceChannel } = require('@discordjs/voice');
 
 client.on(Events.ClientReady, () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
+var userID;
+
 client.on(Events.InteractionCreate, async interaction => {
     if (!interaction.isChatInputCommand()) return;
 
-    if (interaction.commandName === 'ping') {
-        await interaction.reply('Pong!');
-    }
-
     if (interaction.commandName === 'followme'){
-
+        
         let voiceChannelId = getUserVoiceChannel(interaction.member.voice.channel);
         
         if(voiceChannelId){
             connectVoiceChannel(voiceChannelId);
             interaction.reply("Playing");
-            
+            userID = interaction.user.id;
+            getPlayingSong(interaction.member.presence);
         }else{
-            client.channels.send("Devi essere connesso a un canale vocale");
+            interaction.reply("Devi essere connesso a un canale vocale");
         }
+        
+    }
+    
+});
+
+client.on(Events.PresenceUpdate, async (oldPresence, newPresence)  => {
+
+    if(newPresence.userId == userID){
 
     }
-
+    
 });
 
 function getUserVoiceChannel(voice){
     if(voice != null)
-        return voice;
+    return voice;
 }
 
 function connectVoiceChannel(channel){
     
     console.log(`Joined Voice Channel: "${channel.name}"`);
-
     const player = createAudioPlayer();
 
     const connection = joinVoiceChannel({
@@ -55,8 +63,20 @@ function connectVoiceChannel(channel){
 
 }
 
-function getPlayingSong(){
+function getPlayingSong(presence){
 
+    if(presence.status != 'online') return;
+    
+    var activities = presence.activities;
+    
+    activities.forEach(activity => {
+        if(activity.name == 'Spotify'){
+            console.log(`listening to "${activity.details}" by "${activity.state}" at $()`);
+            return activity.details, activity.state;
+        }
+    });
+    
+    return;
 }
 
 function downloadSong(){
