e92319aecd2bd65e4423651bbea79366528ae833
mirco0 <mircomicromac@gmail.com>
2023-01-26 15:27:25 +0100

    Added Youtube input for player

--- app.js
+++ app.js
@@ -3,159 +3,196 @@ const play = require('play-dl')
 
 const { Client, GatewayIntentBits, GatewayDispatchEvents, Events, Colors } = require('discord.js');
 
-const client = new Client({ intents: [GatewayIntentBits.GuildMessages,GatewayIntentBits.Guilds,GatewayIntentBits.GuildVoiceStates 
-    ,GatewayIntentBits.GuildPresences, GatewayIntentBits.MessageContent, GatewayIntentBits.GuildMembers]});
+const client = new Client({ intents: [GatewayIntentBits.GuildMessages,GatewayIntentBits.Guilds,GatewayIntentBits.GuildVoiceStates ,GatewayIntentBits.GuildPresences, GatewayIntentBits.MessageContent, GatewayIntentBits.GuildMembers]});
     
     const { generateDependencyReport, AudioPlayerStatus, createAudioPlayer, createAudioResource, getVoiceConnection, joinVoiceChannel } = require('@discordjs/voice');
     
     client.on(Events.ClientReady, () => {
         console.log(`Logged in as ${client.user.tag}!`);
-        const player = createAudioPlayer();
 });
 
 const player = createAudioPlayer();
-
-var userID,channelId,voiceChannelId,channel;
+var queue = [];
+/**
+ * userID used to follow user's presence
+ * voiceChannelID used to connect to voice channel
+ * channel used to send messages where the command was executed
+ * VoiceConnection used to stream audio in a VoiceChannel
+ */
+var userID,voiceChannelID,channel,VoiceConnection;
 
 //QUICK FIX
 var times = 0;
-//TODO:
-/*
-    0. remove quick fix asap
-    1. make the music seekable
-    2. add pause option
-    3. make the bot automatically disconnect when idle for too long
-    4. reafctoring
-
-*/
 
 client.on(Events.InteractionCreate, async interaction => {
     if (!interaction.isChatInputCommand()) return;
-    if (interaction.commandName === 'followme'){
-        
-        /*
+    
+    resetVariables();
+    channel = interaction.channel;
+    voiceChannelID = interaction.member.voice.channel;
+    userID = interaction.user.id;
 
-        > check if the user is in a voice channel ✓
-        > joins it ✓
-        > gets update about it's spotify presence (seek if needed) 
-        > plays the song ✓
-        
-        < if paused for one minute leave the voice channel ✓
-        
-        */
-        channel = interaction.channel;
-        voiceChannelId = getUserVoiceChannel(interaction.member.voice.channel);
-        channelId = interaction.channelId;
-        if(voiceChannelId){
-
-            userID = interaction.user.id;
+    if(!voiceChannelID){
+        interaction.reply({content: "Devi essere connesso a un canale vocale",ephemeral: true});
+        // resetVariables();
+        return;
+    }
+    
+    switch(interaction.commandName){
+        case 'followme':
             
+            VoiceConnection = connectVoiceChannel(voiceChannelID);
             var presence = interaction.member.presence;
-            fetchAndPlay(voiceChannelId,presence,
-                (song,messageInfo) => {
-                    createPlayingSongMessage(song,messageInfo,
-                        embed => {
-                            interaction.reply({embeds:[embed]});
-                        });
-                },
-                error => {
+            var song = getPlayingSong(presence);
+
+            findSong(song)
+                .then( (result) => { 
+                    var playerPromise = playSong(result.url);
+                    playerPromise.catch((error)=> {console.log(error)});
+                
+                    var embed = createPlayingSongMessage(song,result);
+                    interaction.reply({embeds:[embed]});
+                    }
+                )
+                .catch( (error) => {
                     interaction.reply({content: error,ephemeral: true});
-                }
-            );
-        
-        }else{
-            interaction.reply({content: "Devi essere connesso a un canale vocale",ephemeral: true});
-        }
-        
+                } );
+
+
+        break;
+
+        case 'play':
+
+            var connection = connectVoiceChannel(voiceChannelID);
+            var url = interaction.options.getString('input');
+            
+            var videoFromURL = getSongFromURL(url);
+            videoFromURL.then( (result) => {
+                var playerPromise = playSong(connection,url)
+                    .then()
+                    .catch((error)=> {console.log(error)});
+
+                var embed = createPlayingSongMessage(result.song,result.messageInfo);
+                interaction.reply({embeds:[embed]});
+            })
+                .catch( (error)=> {console.log(error)});
+
+            break;
     }
-    //TODO: APPLY DRY
-    if (interaction.commandName === 'play'){
-        resetVariables();
-        channel = interaction.channel;
-        voiceChannelId = getUserVoiceChannel(interaction.member.voice.channel);
-        if(voiceChannelId){
-            manualPlay(voiceChannelId,interaction.options.getString('input'),
-                (song,messageInfo) => {
-                    createPlayingSongMessage(song,messageInfo,
-                        embed => {
-                            interaction.reply({embeds:[embed]});
-                        });
-                },
-                error => {
-                    interaction.reply({content: error,ephemeral: true});
-                });
-        }else{
-            interaction.reply({content: "Devi essere connesso a un canale vocale",ephemeral: true});
-        }
-    }
+
 });
 
 client.on(Events.PresenceUpdate, async (oldPresence, newPresence)  => {
-    
-    /*
-    check if new presence comes from the same users to that the bot is following
-    */
     times = (times+=1)%2;   //Quick FIX remove asap
     if(times == 0) return   //Quick FIX remove asap
     if(newPresence.userId == userID){
+        
+        var song = getPlayingSong(newPresence);
 
-        fetchAndPlay(voiceChannelId, newPresence, 
-            (song,messageInfo) => {
-                createPlayingSongMessage(song,messageInfo,embed => {
-                    channel.send({embeds:[embed]});
-                    
-                });
-            },
-            error => {
-            // channelId.send(error); temporarly disabled
-        });
+        findSong(song)
+            then( (result) => { 
+                var playerPromise = playSong(result.url);
+                playerPromise.catch((error)=> {console.log(error)});
+            
+                var embed = createPlayingSongMessage(song,result);
+                channel.send({embeds:[embed]});
+                }
+            )
+            .catch( (error) => {
+                channel.send(error);
+            } );
+
+            
+
     }
-    
 });
 
-async function fetchAndPlay(voiceChannelId,presence, callback, error){
-    var messageInfo = {
-        url: undefined,
-        thumbnail: undefined,
-        duration: undefined
+/**
+ * 
+ * @param {*} voiceChannelID
+ * @param {*} presence
+ * @returns
+ */
+async function followSpotifyPresence(){
+
+}
+
+/**
+ * Gets in input a song object and searches it on youtube
+ * @param {*} song
+ * @returns a messageInfo object
+ */
+async function findSong(song){
+    if(!song.name){
+        return Promise.reject("Devi ascoltare una canzone su spotify");
     }
 
-    var connection = connectVoiceChannel(voiceChannelId);
-    var song = getPlayingSong(presence);
+    let searched = await play.search(`${song.name} ${song.author} ${process.env.SEARCH}`, { limit: 1 })
 
-    if(song.name){
-        
-        await findSong(song,messageInfo);
-        console.log(`found: ${messageInfo.url}`);
+    var video = searched[0];
 
-        playSong(connection,messageInfo.url);
-
-        callback(song,messageInfo);
-
+    if(video == undefined){
+        return Promise.reject("Video not found");
     }else{
-        error("Devi ascoltare una canzone su spotify");
+
+        return Promise.resolve( {
+            url: video.url,
+            thumbnail: video.thumbnails[0].url,
+            duration: video.durationRaw
+            }
+        );
     }
 }
 
-async function manualPlay(voiceChannelId,url,callback,error){
-    var messageInfo = {
-        url: undefined,
-        thumbnail: undefined,
-        duration: undefined
+/**
+ * Gets in input a url string and creates a song and a messageInfo object
+ * @param {string} url 
+ * @param {*} error 
+ * @returns a messageInfo object
+ */
+async function getSongFromURL(url){
+    let video =  await play.video_info(url)
+    if(!video || !video.video_details.channel){
+     return Promise.reject("Video non disponibile");
     }
 
-    var connection = connectVoiceChannel(voiceChannelId);
-    playSong(connection,url);
-    callback(connection,url);
+    let messageInfo = {
+        url: url,
+        thumbnail: video.video_details.thumbnails[0].url,
+        duration: video.video_details.durationRaw
+    }
+    
+    let song = {
+        name: video.video_details.title,
+        author: video.video_details.channel.name
+    }
 
+ return Promise.resolve({song: song,messageInfo: messageInfo});
 }
-function getUserVoiceChannel(voice){
-    if(voice != null)
-    return voice;
+
+/**
+ * 
+ * @param {string} url
+ */
+async function playSong(url){
+    let stream = await play.stream(url);
+    let resource = createAudioResource(stream.stream, {
+        inputType: stream.type
+    });
+
+    VoiceConnection.subscribe(player);
+    player.play(resource);
+    
+    player.on(AudioPlayerStatus.Idle, () => {
+
+        setTimeout(checkAndDisconnect,60000);
+    });
+
+    return Promise.resolve();
 }
 
 function connectVoiceChannel(channel){
-    
+
     console.log(`Joined Voice Channel: "${channel.name}"`);
     const connection = joinVoiceChannel({
         channelId: channel.id,
@@ -166,14 +203,20 @@ function connectVoiceChannel(channel){
     return connection;
 }
 
-/*
-return song object {name, author} or undefined
-*/
+function createPlayingSongMessage(song,messageInfo){
+    const embed = {
+        "type": "rich",
+        "title": `Now Playing`,
+        "description": `[${song.name}](${messageInfo.url}) by ${song.author} [${messageInfo.duration}]`,
+        thumbnail: {
+            url: messageInfo.thumbnail,
+        }
+    }
+    return embed;
+}
+
 function getPlayingSong(presence){
 
-    /*
-    if not online disconnect and logout(?) 
-    */
     var song =  {
         name: undefined,
         author: undefined
@@ -185,7 +228,6 @@ function getPlayingSong(presence){
     
     activities.forEach(activity => {
         if(activity.name == 'Spotify'){
-            //console.log(`listening to "${activity.details}" by "${activity.state}" at $()`);
             song.name = activity.details.replace('-',"");
             song.author = activity.state.replace(';',",");
             return song;
@@ -195,57 +237,17 @@ function getPlayingSong(presence){
     return song;
 }
 
-async function findSong(song,messageInfo){
-    
-    console.log(`searching query "${song.name} ${song.author} ${process.env.SEARCH}"`);
-    let searched = await play.search(`${song.name} ${song.author} ${process.env.SEARCH}`, { limit: 1 });
-
-    var video = searched[0];
-    if(video == undefined)  console.log("Song not found");
-
-    messageInfo.url = video.url;
-    messageInfo.thumbnail = video.thumbnails[0].url;
-    messageInfo.duration = video.durationRaw;
-}
-
-async function playSong(connection, url){
-    
-    let stream = await play.stream(url);
-    let resource = createAudioResource(stream.stream, {
-        inputType: stream.type
-    });
-
-    connection.subscribe(player);
-    player.play(resource);
-    
-    //Check 60 seconds after the last track finished to quit the voice channel
-    player.on(AudioPlayerStatus.Idle, () => {
-        setTimeout(checkAndDisconnect,60000,connection,player);
-    });
-}
-
-async function createPlayingSongMessage(song,messageInfo,callback){
-    const embed = {
-            "type": "rich",
-            "title": `Now Playing`,
-            "description": `[${song.name}](${messageInfo.url}) by ${song.author} [${messageInfo.duration}]`,
-            thumbnail: {
-                url: messageInfo.thumbnail,
-            }
-        }
-        callback(embed);
-}
-
-function checkAndDisconnect(connection,player){
+function checkAndDisconnect(){
     if(player.state.status != AudioPlayerStatus.Playing){
         connection.disconnect();
-        console.log(`Disconnected Voice Channel: "${voiceChannelId.name}"`);
+        console.log(`Disconnected Voice Channel: "${voiceChannelID.name}"`);
+        channel.send("Disconnesso dal canale Vocale per Inattività");
         resetVariables();
     }
 }
 
 function resetVariables(){
-    userID = channelId = voiceChannelId = channel = undefined;
+    userID = voiceChannelID = channel = undefined;
     times = 0;
 }
 
